ff38d3528872df4307585ff14d6117cc9a3f3cd9
Dirk Lehmann <Dirk.Lehmann@digitalfabrix.de>
2025-09-18 10:35:40 +0200

    GetProductByCategory

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+
+// Nicht verwendet!
+//public record GetProductByCategoryRequest();
+
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (ISender sender, string category) =>
+        {
+            var products = await sender.Send(new GetProductByCategoryQuery(category));
+            var response = products.Adapt<GetProductByCategoryResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProductByCategory")
+        .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Category")
+        .WithDescription("Get Product By Category");
+    }
+}
