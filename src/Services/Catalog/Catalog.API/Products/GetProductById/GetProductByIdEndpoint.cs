71a897497f3b21d127782516fb6c92faa0b05e9f
Dirk Lehmann <Dirk.Lehmann@digitalfabrix.de>
2025-09-18 10:17:54 +0200

    GetProductById

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.GetProductById;
+
+// Nicht verwendet!
+public record GetProductByIdRequest();
+
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (ISender sender, Guid id) =>
+        {
+            var product = await sender.Send(new GetProductByIdQuery(id));
+            var response = product.Adapt<GetProductByIdResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get Product By Id");
+    }
+}
