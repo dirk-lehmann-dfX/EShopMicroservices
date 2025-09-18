302c1a3dbf0e05316ce37018bbd259701fc73463
Dirk Lehmann <Dirk.Lehmann@digitalfabrix.de>
2025-09-18 09:18:38 +0200

    GetProducts

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,28 @@
+﻿namespace Catalog.API.Products.GetProducts;
+
+
+// Wird nicht verwendet!
+public record GetProductsRequest();
+
+public record GetPoductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var products = await sender.Send(new GetProductsQuery());
+
+            var response = products.Adapt<GetPoductsResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<GetPoductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+    }
+}
+
---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+
+// !Wichtig: Der Name "Products" ist so gewählt, dass er mit dem Property-Namen in GetPoductsResponse übereinstimmt.
+public record GetProductsResult(IEnumerable<Product> Products);
+
+internal class GetProductsQueryHandler(IDocumentSession documentSession, ILogger<GetProductsQueryHandler> logger)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        // query wird nicht verwendet! => Separation
+        logger.LogInformation("GetProductQueryHandler.Handle clled with {@Query}", query);
+
+        var products = await documentSession.Query<Product>().ToListAsync(cancellationToken);
+
+        return new GetProductsResult(products);
+    }
+}
