2ed764d7ced1917e5382b2861b699fe6febb6c05
Dirk Lehmann <Dirk.Lehmann@digitalfabrix.de>
2025-09-23 15:12:01 +0200

    Validatoren DeleteProduct + UpdateProduct

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -6,6 +6,20 @@ public record UpdateProductCommand(Guid Id, string Name, List<string> Category, 
 
 public record UpdateProductResult(bool IsSuccess);
 
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(command => command.Id)
+            .NotEmpty().WithMessage("Product Id is required.");
+        RuleFor(command => command.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+        RuleFor(command => command.Price)
+            .GreaterThan(0).WithMessage("Price must be grater than 0.");
+    }
+}
+
 internal class UpdateProductCommandHandler(IDocumentSession documentSession, ILogger<UpdateProductCommandHandler> logger) :
     ICommandHandler<UpdateProductCommand, UpdateProductResult>
 {
