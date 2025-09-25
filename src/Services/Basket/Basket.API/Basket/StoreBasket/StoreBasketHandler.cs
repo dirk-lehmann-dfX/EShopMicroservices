427fa19179dc855dce739f21ce661bb9f97c2811
Dirk Lehmann <Dirk.Lehmann@digitalfabrix.de>
2025-09-25 15:57:35 +0200

    Basket.API - StoreBasket

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,5 +1,26 @@
 ﻿namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketHandler
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 {
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(command => command.Cart)
+            .NotNull().WithMessage("Cart is required.");
+        RuleFor(command => command.Cart.UserName)
+            .NotEmpty().WithMessage("UserName is required.");
+    }
+}
+
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+
+        return new StoreBasketResult(command.Cart.UserName);
+    }
 }
