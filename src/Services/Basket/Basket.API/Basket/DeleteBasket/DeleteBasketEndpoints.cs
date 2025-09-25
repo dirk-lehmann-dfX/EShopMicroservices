ba4f19274f35bf7ef73dd48a160b97e07e23788a
Dirk Lehmann <Dirk.Lehmann@digitalfabrix.de>
2025-09-25 16:04:09 +0200

    Basket.API - DeleteBasket

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
@@ -1,5 +1,26 @@
 ﻿namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketEndpoints
+public record DeleteBasketRequest(string UserName);
+
+public record DeleteBasketResponse(bool IsSuccess);
+
+public class DeleteBasketEndpoints : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{userName}", async (ISender sender, string userName) =>
+        {
+            var command = new DeleteBasketCommand(userName);
+            var result = await sender.Send(command);
+            var response = result.Adapt<DeleteBasketResponse>();
+
+            return Results.Ok(response);
+        })
+            .WithName("DeleteBasket")
+            .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Basket")
+            .WithDescription("Delete Basket");
+    }
 }
