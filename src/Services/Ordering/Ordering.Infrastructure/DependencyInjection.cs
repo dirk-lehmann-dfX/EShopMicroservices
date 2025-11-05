4093415bb70ddf35f2634797ef37e03e046b6597
Dirk Lehmann <Dirk.Lehmann@digitalfabrix.de>
2025-11-05 14:49:31 +0100

    Ordering - Migrations

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -7,13 +7,13 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("Database");
+        var connectionString = configuration.GetConnectionString("OrderDatabase");
 
         // Add infrastructure services registrations here
-        //services.AddDbContext<ApplicationDbContext>(options =>
-        //{
-        //    options.UseSqlServer(connectionString);
-        //});
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
         //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
         return services;
