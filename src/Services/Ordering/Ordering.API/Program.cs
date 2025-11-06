3fa608571caac2593ae9bf8d1585ab3acbbf06b4
Dirk Lehmann <Dirk.Lehmann@digitalfabrix.de>
2025-11-06 10:29:03 +0100

    Ordering - Seed SQL-Data

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,12 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 
 
 app.Run();
---
+++ src/Services/Ordering/Ordering.Infrastructure/Extensions/InitialData.cs
@@ -0,0 +1,66 @@
+﻿namespace Ordering.Infrastructure.Extensions;
+
+internal class InitialData
+{
+    private static Guid _customerId1 = Guid.NewGuid();
+    private static Guid _customerId2 = Guid.NewGuid();
+
+    private static Guid _product1 = Guid.NewGuid();
+    private static Guid _product2 = Guid.NewGuid();
+    private static Guid _product3 = Guid.NewGuid();
+    private static Guid _product4 = Guid.NewGuid();
+
+    public static IEnumerable<Customer> Customers =>
+        new List<Customer>
+        {
+            Customer.Create(CustomerId.Of(_customerId1), "mehmet", "mehmet@gmail.com"),
+            Customer.Create(CustomerId.Of(_customerId2), "john", "john@gmail.com"),
+        };
+
+    public static IEnumerable<Product> Products =>
+        new List<Product>
+        {
+            Product.Create(ProductId.Of(_product1), "IPhone X", 500),
+            Product.Create(ProductId.Of(_product2), "Samsung 10", 400),
+            Product.Create(ProductId.Of(_product3), "Huawei Plus", 650),
+            Product.Create(ProductId.Of(_product4), "Xiaomi Mi", 450)
+        };
+
+    public static IEnumerable<Order> OrdersWithOrderItems
+    {
+        get
+        {
+            var address1 = Address.Of("mehmet", "ozkaya", "mehmet@gmail.com", "Bahcelievler No:4", "Turjey", "Istanbul", "38050");
+            var address2 = Address.Of("john", "doe", "john@gmail.com", "Broadway No:1", "England", "Nottingham", "08050");
+
+            var payment1 = Payment.Of("mehmet", "55555555555555544", "12/25", "123", 1);
+            var payment2 = Payment.Of("john", "55555555555555544", "06/30", "222", 2);
+
+            var order1 = Order.Create(
+                OrderId.Of(Guid.NewGuid()),
+                CustomerId.Of(_customerId1),
+                OrderName.Of("Ord 1"),
+                shippingAddress: address1,
+                billingAddress: address1,
+                payment1);
+            order1.Add(
+                ProductId.Of(_product1), 2, 500);
+            order1.Add(
+                ProductId.Of(_product2), 1, 400);
+
+            var order2 = Order.Create(
+               OrderId.Of(Guid.NewGuid()),
+               CustomerId.Of(_customerId2),
+               OrderName.Of("Ord 2"),
+               shippingAddress: address2,
+               billingAddress: address2,
+               payment2);
+            order2.Add(
+                ProductId.Of(_product3), 1, 650);
+            order2.Add(
+                ProductId.Of(_product4), 2, 450);
+
+            return new List<Order> { order1, order2 };
+        }
+    }
+}
