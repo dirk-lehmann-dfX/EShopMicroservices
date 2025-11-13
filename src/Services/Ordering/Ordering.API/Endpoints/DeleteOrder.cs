d04af344e550980b5c625cb3de0bfc51257be73e
Dirk Lehmann <Dirk.Lehmann@digitalfabrix.de>
2025-11-13 11:48:57 +0100

    Ordering - MinimalApi CreateOrder, Carter

---
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.API.Endpoints;
+
+// Accepts a order Id as a parameter
+// Constructs a DeleteOrderCommand
+// Use MediatR to send the command to the corresponding handler
+// Returns a success or not found response
+public class DeleteOrder
+{
+}
---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.API.Endpoints;
+
+// Accepts a customer Id
+// Uses a GetOrdersByCustomerQuery to fetch orders
+// Returns a list of orders for that customer
+public class GetOrdersByCustomer
+{
+}
